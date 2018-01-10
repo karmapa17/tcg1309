74e8097a7d38eaf814c27f0ce962807d059d113a
kmsheng <kmsh3ng@gmail.com>
2018-01-10 10:29:13 +0800

    add file bcd.h

---
+++ src/bcd.h
@@ -0,0 +1,10 @@
+#define BCDMAX 20    // was 24
+
+void l2bcd(signed char *, int);
+int bcd2l(signed char *);
+void addbcd(signed char *, signed char *, signed char *);
+int compabsbcd(signed char *, signed char *);
+void subbcd(signed char *, signed char *, signed char *);
+void mulbcdl(signed char *, signed char *, int);
+void divbcdl(signed char *, signed char *, int);
+void modbcdl(signed char *, signed char *, int);
